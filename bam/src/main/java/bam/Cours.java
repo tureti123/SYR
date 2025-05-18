7665a319ef8910c6852919da9abf88bc58b9ddb3
tureti123 <132614552+tureti123@users.noreply.github.com>
2025-05-18 18:30:41 +0100

    Update Cours.java

--- bam/src/main/java/bam/Cours.java
+++ bam/src/main/java/bam/Cours.java
@@ -3,6 +3,8 @@ package bam;
 import java.util.Random;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+
 
 public class Cours {
 int Id;
@@ -11,6 +13,7 @@ String Type;
 Salle sdc;
 Enseignant enseignant;
 ArrayList<Horaire> LH;
+HashMap<LocalDateTime,LocalDateTime> hor =new HashMap<>();
 public Cours(String Nom,String Type) {
 	Random r = new Random();
 	int x = r.nextInt(10000);
