a3eaca0d32511301864f7a89a84bc7d37a30fac7
Soo454 <sofianetoumiat5@gmail.com>
2025-05-22 21:14:33 +0200

    Add files via upload

---
+++ bam/src/main/java/bam/register_Controller.java
@@ -0,0 +1,41 @@
+package bam;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class register_Controller {
+	@FXML
+	private void goToRegister(ActionEvent event) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/fxml/register.fxml"));
+			Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	private void goTologin(ActionEvent event) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+			Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
